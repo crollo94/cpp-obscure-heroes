51d9890213831cf0e56acc97dbbd4223d2395f93
crollo94 <kelly.s826508@stu.sanjac.edu>
2025-04-21 00:17:30 +0000

    using a selection sort instead of the last way after reading book some more. also useing cctype for tolower and toupper

--- superhero_database.cpp
+++ superhero_database.cpp
@@ -6,7 +6,7 @@
 
 #include<iostream>
 #include<string>
-#include<algorithm>
+#include<cctype> // for tolower and toupper
 
 using namespace std;
 
@@ -27,28 +27,34 @@ void caseoption();
 
 string converttolower(string heroname)
 {
-    transform(heroname.begin(), heroname.end(), heroname.begin(), ::tolower);
+    for (int i = 0; i < heroname.length(); i++)
+    {
+        heroname[i] = tolower(heroname[i]);
+    }
     return heroname;
 }
 
 string converttoupper(string text)
 {
-    transform(text.begin(), text.end(), text.begin(), ::toupper);
+    for (int i = 0; i < text.length(); i++)
+    {
+        text[i] = toupper(text[i]);
+    }
     return text;
 }
 
 void loadSuperherodata()
 {
-    name[0] = "Squirrel Girl"; power[0] = "Control Squirrels"; weakness[0] = "Time Limits"; yearUni[0][0] = "1992"; yearUni[0][1] = "Marvel";
-    name[1] = "Zeitgeist"; power[1] = "Acidic Vomit"; weakness[1] = "His Own Acid"; yearUni[1][0] = "2001"; yearUni[1][1] = "Marvel";
-    name[2] = "Ragman"; power[2] = "Absorbs Souls"; weakness[2] = "Evil Soul Overload"; yearUni[2][0] = "1976"; yearUni[2][1] = "DC";
-    name[3] = "Matter-Eater Lad"; power[3] = "Can Eat Anything"; weakness[3] = "Stomach Aches"; yearUni[3][0] = "1962"; yearUni[3][1] = "DC";
-    name[4] = "Zan"; power[4] = "Transforms into Water"; weakness[4] = "Dry Environments"; yearUni[4][0] = "1977"; yearUni[4][1] = "DC";
-    name[5] = "Color Kid"; power[5] = "Changes Object Colors"; weakness[5] = "Color Blindness"; yearUni[5][0] = "1966"; yearUni[5][1] = "DC";
-    name[6] = "Doorman"; power[6] = "Create Doorways"; weakness[6] = "Needs a Wall"; yearUni[6][0] = "1989"; yearUni[6][1] = "Marvel";
-    name[7] = "Bouncing Boy"; power[7] = "Inflates and Bounces"; weakness[7] = "Sharp Objects"; yearUni[7][0] = "1961"; yearUni[7][1] = "DC";
-    name[8] = "Dogwelder"; power[8] = "Welds Dogs to Faces"; weakness[8] = "Dog Shortage"; yearUni[8][0] = "1996"; yearUni[8][1] = "DC";
-    name[9] = "Ruby Thursday"; power[9] = "Morphing Head"; weakness[9] = "Head Damage"; yearUni[9][0] = "1976"; yearUni[9][1] = "Marvel";
+    name[0] = "Squirrel Girl"; power[0] = "  | Control Squirrels"; weakness[0] = "      | Time Limits"; yearUni[0][0] = "          | 1992"; yearUni[0][1] = "   |  Marvel";
+    name[1] = "Zeitgeist"; power[1] = "      | Acidic Vomit"; weakness[1] = "           | His Own Acid"; yearUni[1][0] = "         | 2001"; yearUni[1][1] = "   |  Marvel";
+    name[2] = "Ragman"; power[2] = "         | Absorbs Souls"; weakness[2] = "          | Evil Soul Overload"; yearUni[2][0] = "   | 1976"; yearUni[2][1] = "   |    DC";
+    name[3] ="Matter-Eater"; power[3]= "   | Can Eat Anything"; weakness[3] = "       | Stomach Aches"; yearUni[3][0] = "        | 1962"; yearUni[3][1] = "   |    DC";
+    name[4] = "Zan"; power[4] = "            | Transforms into Water"; weakness[4] = "  | Dry Environments"; yearUni[4][0] = "     | 1977"; yearUni[4][1] = "   |    DC";
+    name[5] = "Color Kid"; power[5] = "      | changes Object Colors  "; weakness[5] = "| Color Blindness "; yearUni[5][0] = "     | 1966 "; yearUni[5][1] = "  |    DC";
+    name[6] = "Doorman"; power[6] = "        | Create Doorways"; weakness[6] = "        | Needs a Wall"; yearUni[6][0] = "         | 1989"; yearUni[6][1] = "   |  Marvel";
+    name[7] = "Bouncing Boy"; power[7] = "   | Inflates and Bounces "; weakness[7] = "  | Sharp Objects"; yearUni[7][0] = "        | 1961"; yearUni[7][1] = "   |    DC";
+    name[8] = "Dogwelder"; power[8] = "      | Welds Dogs to Faces"; weakness[8] = "    | Dog Shortage"; yearUni[8][0] = "         | 1996"; yearUni[8][1] = "   |    DC";
+    name[9] = "Ruby Thursday"; power[9] = "  | Morphing Head"; weakness[9] = "          | Head Damage"; yearUni[9][0] = "          | 1976"; yearUni[9][1] = "   |  Marvel";
 }
 
 void searchbyname()
@@ -106,21 +112,42 @@ void sortbyletter()
 {
     for (int i=0; i < arraySize - 1; i++)
     {
-        for(int j = i + 1; j < arraySize; j++)
+        int min = i;
+        for (int j = i + 1; j < arraySize; j++)
         {
-            if(name[i] > name[j])
+            if(name[j] < name[min])
             {
-                swap(name[i], name[j]);
-                swap(power[i], power[j]);
-                swap(weakness[i], weakness[j]);
-                swap(yearUni[i][0], yearUni[j][0]);
-                swap(yearUni[i][1], yearUni[j][1]);
+                min = j;
 
             }
         }
+
+        string temp;
+
+        temp = name[i];
+        name[i] = name[min];
+        name[min] = temp;
+
+        temp = power[i];
+        power[i] = power[min];
+        power[min] = temp;
+
+        temp = weakness[i];
+        weakness[i] = weakness[min];
+        weakness[min] = temp;
+
+        temp = yearUni[i][0];
+        yearUni[i][0] = yearUni[min][0];
+        yearUni[min][0]= temp;
+
+        temp = yearUni[i][1];
+        yearUni[i][1] = yearUni[min][1];
+        yearUni[min][1] = temp;
     }
 
     cout << "super heros sorted! ";
+    cout << "\nName            | Superpower              |   Weakness            | Year    |  Universe" << endl;
+    cout << "-------------------------------------------------------------------------------------------" << endl;
     for (int i = 0; i < arraySize; i++)
     {
         cout << name[i] << " " << power[i]<< " " << weakness[i] << " " << yearUni[i][0] << " " << yearUni[i][1] << endl;
